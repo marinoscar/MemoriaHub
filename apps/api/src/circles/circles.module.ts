4e38d403f462f5f7081a8e9ad8b00e4c13b800c8
Oscar Marin <oscar@marin.cr>
2026-06-14 19:18:04 +0200

    feat(api): wire CirclesService and CirclesController into CirclesModule

--- apps/api/src/circles/circles.module.ts
+++ apps/api/src/circles/circles.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AllowlistModule } from '../allowlist/allowlist.module';
 import { CircleMembershipService } from './circle-membership.service';
 import { CircleMemberGuard } from './guards/circle-member.guard';
+import { CirclesService } from './circles.service';
+import { CirclesController } from './circles.controller';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [CircleMembershipService, CircleMemberGuard],
-  exports: [CircleMembershipService, CircleMemberGuard],
+  imports: [PrismaModule, AllowlistModule],
+  controllers: [CirclesController],
+  providers: [CircleMembershipService, CircleMemberGuard, CirclesService],
+  exports: [CircleMembershipService, CircleMemberGuard, CirclesService],
 })
 export class CirclesModule {}
