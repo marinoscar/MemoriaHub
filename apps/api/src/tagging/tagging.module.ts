2ff3c8464e4e29ffb18b1eca771a7be6d6f59c6d
Oscar Marin <oscar@marin.cr>
2026-06-19 07:10:15 +0200

    feat(api): wire TaggingModule into app

---
+++ apps/api/src/tagging/tagging.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { EnrichmentModule } from '../enrichment/enrichment.module';
+import { AiModule } from '../ai/ai.module';
+import { StorageProvidersModule } from '../storage/providers/storage-providers.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { CirclesModule } from '../circles/circles.module';
+import { AutoTaggingHandler } from './auto-tagging.handler';
+import { AutoTaggingService } from './auto-tagging.service';
+import { TaggingEnqueueListener } from './tagging-enqueue.listener';
+import { TaggingController } from './tagging.controller';
+import { TagLabelsController } from './tag-labels.controller';
+import { TagLabelsService } from './tag-labels.service';
+
+@Module({
+  imports: [EnrichmentModule, AiModule, StorageProvidersModule, PrismaModule, CirclesModule],
+  controllers: [TaggingController, TagLabelsController],
+  providers: [AutoTaggingHandler, AutoTaggingService, TaggingEnqueueListener, TagLabelsService],
+})
+export class TaggingModule {}
