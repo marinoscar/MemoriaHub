d564702375b1d07a645fb6850cba5f923242523d
Oscar Marin <oscar@marin.cr>
2026-06-20 06:16:18 +0200

    feat(api): wire EnrichmentModule and StorageInsightsHandler into InsightsModule

--- apps/api/src/insights/insights.module.ts
+++ apps/api/src/insights/insights.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SettingsModule } from '../settings/settings.module';
+import { EnrichmentModule } from '../enrichment/enrichment.module';
 import { InsightsService } from './insights.service';
 import { InsightsRefreshTask } from './insights-refresh.task';
 import { InsightsController } from './insights.controller';
+import { StorageInsightsHandler } from './storage-insights.handler';
 
 @Module({
-  imports: [PrismaModule, SettingsModule],
+  imports: [PrismaModule, SettingsModule, EnrichmentModule],
   controllers: [InsightsController],
-  providers: [InsightsService, InsightsRefreshTask],
+  providers: [InsightsService, InsightsRefreshTask, StorageInsightsHandler],
   exports: [InsightsService],
 })
 export class InsightsModule {}
